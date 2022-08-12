60394205d51e79bab583e4d69fcf601bf8f530a7
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-08-12 23:52:33 +0300

    ruby

---
+++ app.rb
@@ -0,0 +1,15 @@
+puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+print "Pass this test, please."
+
+p [1,2,3]
+
+puts "Hello World!"
